62d28fa43e4f419ea3557bd079811c0cff5d787c
shashank2404 <shashanklal43@gmail.com>
2026-04-19 16:57:44 +0530

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy: {
-    //   "/api": "https://bible-automation-1.onrender.com",
-    //   "/ask": "https://bible-automation-1.onrender.com",
-    //   "/random-verse": "https://bible-automation-1.onrender.com",
-    //   "/search": "https://bible-automation-1.onrender.com",
-    //   "/verse": "https://bible-automation-1.onrender.com",
-    // },
+    proxy: {
+      "/api": "https://bible-automation-1.onrender.com",
+      "/ask": "https://bible-automation-1.onrender.com",
+      "/random-verse": "https://bible-automation-1.onrender.com",
+      "/search": "https://bible-automation-1.onrender.com",
+      "/verse": "https://bible-automation-1.onrender.com",
+    },
   },
   appType: "spa",
 });
